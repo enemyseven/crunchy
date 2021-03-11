bed34a2b47e8468a0187e34b1141290b7891a048
enemyseven <enemyseven@users.noreply.github.com>
2021-03-11 11:44:04 -0600

    Add files via upload
    Initial code commit.

---
+++ crunchy.py
@@ -0,0 +1,81 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Name: Crunch Time
+Author: Cody Hill
+Date Created: March 10, 2021
+Last Modified: March 11, 2021
+This Software is released under the MIT License:
+http://www.opensource.org/licenses/mit-license.html
+See LICENSE at root of project for more details.
+(C) 2021 Cody Hill
+"""
+import time
+import os
+import subprocess
+import glob
+
+VERSION = '0.0.1'
+
+# MARK: Global Variables
+
+# Directories
+ffmpeg = "./ffmpeg"
+inputPath = "./input/"
+cachePath = "./cache/"
+commonPath = "./common/"
+outputPath = "./output/"
+
+# MARK: Functions
+
+def makePreview(input, output):
+    if not os.path.isfile(output):
+        execute = [
+            ffmpeg,
+            '-i', input, # Input filename
+            '-ss', '0:00:03', # Skip ahead to
+            '-to', '0:00:13', # copy until
+            '-filter:v',
+            'fps=15, crop=ih/3*4:ih, scale=320:240, format=yuv420p',
+            '-an', # Do not extract audio
+            f'{output}', # output filename
+            '-y' # overwrite output if it exists
+        ]
+        try:
+            print(execute)
+            subprocess.run(execute, check=True)
+        except subprocess.CalledProcessError as e:
+            print("Early exit due to error.")
+            exit()
+    else:
+        print("Warngng: ", output, " already exits.")
+
+
+def main():
+  filenames = []
+  
+  textfiles = glob.glob("filelist.txt")
+  
+  for textfile in textfiles:
+      with open(textfile) as f:
+        filenames.extend(f.read().splitlines())
+
+  for filename in filenames:
+    if filename == "\n":
+        print("Encountered newline.")
+        break
+        
+    # Make target input
+    input = inputPath + filename
+
+    # Make target output
+    pre, ext = os.path.splitext(filename)
+    output = outputPath + pre + "-preview.mp4"
+    pre, ext = os.path.splitext(input)
+    
+    makePreview(input, output)
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
